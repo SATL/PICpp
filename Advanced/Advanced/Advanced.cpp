e520f5d4f42e60b4dea9d5dd9c110403a4811503
Eslem <j.eslem03@gmail.com>
2015-10-18 01:48:45 +0200

    Memory, structs, typedef

---
+++ Advanced/Advanced/Advanced.cpp
@@ -0,0 +1,16 @@
+// Advanced.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "DinamicMemory.h"
+#include "Structures.h"
+
+int main()
+{
+	//DMMain();
+	StructuresMain();
+
+	getchar();
+    return 0;
+}
+
